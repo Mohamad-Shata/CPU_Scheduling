1b7524c08f4133c11552f50957a00fae9ed83b55
ZiadSharaf <115091905+ZiadSharaf@users.noreply.github.com>
2024-12-07 22:47:49 +0200

    Update Process.java

--- src/main/java/org/os/Process.java
+++ src/main/java/org/os/Process.java
@@ -8,14 +8,21 @@ public class Process {
     public int arrivalTime;
     public int waitingTime;
     public int turnaroundTime;
+    public int quantum;
+    public int completionTime;
+
+
 
     public Process(int id, int burstTime, int priority, int arrivalTime) {
         this.id = id;
         this.burstTime = burstTime;
-        this.remainingTime = burstTime; // Used for SRTF
-        this.priority = priority;      // Used for Priority Scheduling
+        this.remainingTime = burstTime;
+        this.priority = priority;
         this.arrivalTime = arrivalTime;
         this.waitingTime = 0;
         this.turnaroundTime = 0;
+        this.quantum = 0;
+        this.completionTime=0;
+
     }
 }
